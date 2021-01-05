ed8d51fcc1cfd6d91c1e0adc31c1ab02e15767f6
daat2 <ibi.olu@gmail.com>
2021-01-05 00:50:17 +0000

    Added shopping bag,urls and template

--- myboutique_ado/settings.py
+++ myboutique_ado/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+     'bag',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
