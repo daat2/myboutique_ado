53d2d7001493d1ea2e452149862e6e9fbc5b91ca
daat2 <ibi.olu@gmail.com>
2021-01-05 19:53:40 +0000

    Updated contex processor

--- bag/views.py
+++ bag/views.py
@@ -19,7 +19,6 @@ def add_to_bag(request, item_id):
     else:
         bag[item_id] = quantity
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
+    request.session['bag'] = bag   
     return redirect(redirect_url)
     
