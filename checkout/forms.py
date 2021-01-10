80ce032b2525a74c642c5bd01ec16b93b78e6025
daat2 <ibi.olu@gmail.com>
2021-01-10 15:50:44 +0000

    Added checkout success logic

--- checkout/forms.py
+++ checkout/forms.py
@@ -37,3 +37,5 @@ class OrderForm(forms.ModelForm):
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
+
+            
